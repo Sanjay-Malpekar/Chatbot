c9cf37335e6ba5de10234b859cb0d6ef98691a91
Sanjay-Malpekar <sanjaymalpekar291@gmail.com>
2021-05-09 21:17:59 +0000

    Modified

--- actions/actions.py
+++ actions/actions.py
@@ -10,11 +10,15 @@ import requests
 
 import firebase_admin
 from firebase_admin import db
+from firebase_admin import firestore
 
 cred_obj = firebase_admin.credentials.Certificate('Firebasekey.json')
 default_app = firebase_admin.initialize_app(cred_obj,{'databaseURL':'https://rasa-78cd0-default-rtdb.firebaseio.com/'})
 ref = db.reference('/')
+ref_firestore = firestore.client()
 
+
+user_id = "A8UXKPR88WG8P"
 
 # import json
 # import os
@@ -110,15 +114,15 @@ def retrieve_data(Category):
     
     for i in range(3):
         cat = 'Sub_Category' + '/' + str(i)
-        result = ref.child('Products').order_by_child(cat).equal_to(Category).limit_to_first(1).get()
+        result = ref.child('Products').order_by_child(cat).equal_to(Category).limit_to_first(5).get()
         if result is not None:
             return list(result.values())
 
 def retrieve_data_from_recommender_api(userid):
-    recommended_products = requests.get("https://5000-coral-dinosaur-mgokyf3b.ws-us04.gitpod.io/{userid}")
+    recommended_products = requests.get("https://5000-green-wolverine-xqc6lqq6.ws-us03.gitpod.io/{userid}")
     
 
-    return recommended_products.json()['Recommended_Products'][:1]
+    return recommended_products.json()['Recommended_Products'][:5]
 
 class ActionRecommender(Action):
 
@@ -132,13 +136,13 @@ class ActionRecommender(Action):
         domain: "DomainDict"
     ) -> List[Dict[Text, Any]]:
         
-        recommend_products = retrieve_data_from_recommender_api("A8UXKPR88WG8P")
+        # user_id = "A8UXKPR88WG8P"
+        #Checking if the user is new 
+        users = ref_firestore.collection('User').where('user_id','==',user_id).stream()
+        
+        if len([user.id for user in users]) > 0:
+            recommend_products = retrieve_data_from_recommender_api(user_id)
 
-        if recommend_products is None:
-            dispatcher.utter_message(
-                text = "You seems a new User So, Here goes our Top 5 products! ;)"
-                )
-        else:
             recommend_products_list =[]
             for product in recommend_products:
                 refer = 'Products' + '/' + str(product)
@@ -151,6 +155,26 @@ class ActionRecommender(Action):
                     image = recommend_products_list[i]['image'][0]
                     )
             dispatcher.utter_message(text="If you liked the products type 1..5 according to the product you want! ;)")
+        else:
+            
+            dispatcher.utter_message(text = "You seems a new User So, Here goes our Top 5 products! ;)")
+            
+            Popular_products = ['B000VRMV1I','B000U6H08O','B000PGJ3IE','B000P4M1F8','B000EHTY0Q']
+ 
+            recommend_products_list =[]
+            for product in Popular_products:
+                refer = 'Products' + '/' + str(product)
+                result = ref.child(refer).get()
+                recommend_products_list.append(result)
+            
+            for i in range(len(recommend_products_list)):
+                dispatcher.utter_message(
+                    text ="Product Title: {}".format(recommend_products_list[i]['title']),
+                    image = recommend_products_list[i]['image'][0]
+                    )
+            dispatcher.utter_message(text="If you liked the products type 1..5 according to the product you want! ;)")
+        
+            
         return[SlotSet("products_list", recommend_products_list)]
 
 class ActionSearchProvider(Action):
@@ -196,9 +220,32 @@ class ActionLookupAddress(Action):
         domain: "DomainDict"
     ) -> List[Dict[Text, Any]]:
         
-        dispatcher.utter_message(
-           text="We could not Found address Can you please enter address here!")
-        return[]
+        # user_id = "A8UXKPR88WG8P"
+        #Checking if the user is new 
+        users = ref_firestore.collection('User').where('user_id','==',user_id).stream()
+
+        if len([user.id for user in users]) > 0:
+            address_list = ref_firestore.collection(u'User').document(user_id).collection(u'address').limit(2).stream()
+            
+            address = []
+            for addr in address_list:
+                address.append(addr.to_dict())
+            
+            if len(address) == 1:
+                dispatcher.utter_message(
+                    text="Here is your lastest address we found please pick one!",
+                    buttons = [{'title': address[0]['address'], 'payload': '/address_found'}])
+            
+            elif len(address) == 2:
+                dispatcher.utter_message(
+                    text="Here is your lastest address we found please pick one!",
+                    buttons = [{'title': address[0]['address'], 'payload': '/address_found'},{'title': address[1]['address'], 'payload': '/address_found'}])
+
+            
+            return[SlotSet("address", address[0]['address'])]
+        else:
+            dispatcher.utter_message(text="We could not Found? Can you please help us with the address here!")
+            return[]
 
 class ActionAddAddress(Action):
 
@@ -213,10 +260,13 @@ class ActionAddAddress(Action):
     ) -> List[Dict[Text, Any]]:
 
         current_address = tracker.latest_message['text']
-        
+
+        # user_id = "A8UXKPR88WG8P"
+        ref_firestore.collection(u'User').document(user_id).set({u'user_id': user_id})
+        ref_firestore.collection(u'User').document(user_id).collection(u'address').document().set({u'address': current_address})
         
         dispatcher.utter_message(
-           text="Address_added!")
+           text="Your Address has been added Successfully!")
         return[SlotSet("address", current_address)]
     
 class ActionPlaceOrder(Action):
@@ -231,7 +281,27 @@ class ActionPlaceOrder(Action):
         domain: "DomainDict"
     ) -> List[Dict[Text, Any]]:
 
+        # user_id = "A8UXKPR88WG8P"
+
+        address_selected = tracker.get_slot("address")
+
         Payment_selected = tracker.get_slot("payment_type")
+
+        option_selected = tracker.get_slot('option_select')
+
+        product = tracker.get_slot('products_list')
+
+        dict_data = {'ProdID': product[int(option_selected)-1]['ProdID'],
+        'payment_type': Payment_selected,
+        'brand': product[int(option_selected)-1]['brand'],
+        'address': address_selected,
+        'image': product[int(option_selected)-1]['image'],
+        'price': product[int(option_selected)-1]['price'],
+        'rating': product[int(option_selected)-1]['rating'],
+        'title': product[int(option_selected)-1]['title']}
+
+        ref_firestore.collection(u'User').document(user_id).set({u'user_id': user_id})
+        ref_firestore.collection(u'User').document(user_id).collection(u'cart').document().set(dict_data)
 
         dispatcher.utter_message(text="Your Order has been Placed! Thank You :)")
         
